ffb7d941c86e9ca9d9d90cfdb84492f0fa3b3477
Pierre BOURRY <pierre@pierrebourry.com>
2026-04-25 18:46:21 +0200

    Bump version: 1.0.1 → 1.0.2

--- cli/agent_linux_cli.py
+++ cli/agent_linux_cli.py
@@ -26,7 +26,7 @@ except ImportError:
 
 # ── Constants ─────────────────────────────────────────────────────────────────
 
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 
 VENV_DIR = "/opt/agent-linux/venv"
 
